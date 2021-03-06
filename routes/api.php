92db62b2b0565005d3d402ed6b999db4c55d905a
Roberto Cruz <al221910393@gmail.com>
2021-03-06 09:15:20 -0600

    end poit post category

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+//Routes category
+Route::apiResource('category','\App\Http\Controllers\categoryController');
+//post
+Route::apiResource('post','\App\Http\Controllers\PostController');
